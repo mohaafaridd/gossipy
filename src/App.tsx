ec214ac15eeef50a2b70eda9ad23fb18e26f6baa
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-02-10 06:16:58 +0200

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { FaThumbsUp } from 'react-icons/fa'
+import './App.css'
 
-const App = () => {
+const App: FC = ({ children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider>
+      <CSSReset />
+      <h1>
+        First commit yay <FaThumbsUp />
+      </h1>
+      {children}
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
