9933258852c2ddd0a9afa40666f1ef5aeafec9c0
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-03-07 14:41:21 +0200

    exported topics route to explore page to help separating code

---
+++ src/pages/Explore.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Topics from '../components/Topics'
+
+const Explore = () => {
+  return <Topics sort='HOT' dateRange='TODAY' />
+}
+
+export default Explore
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,17 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import Nav from '../components/Nav'
-import Topic from '../components/Topic'
-import Topics from '../components/Topics'
 
-import { Topic as ITopic } from '../interfaces/Topic'
-import moment from 'moment'
+// Pages
+import Explore from './Explore'
+
 const Home = () => {
-  const topic: ITopic = {
-    id: 'topic123',
-    title: 'Test Topic',
-    content:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique velit id officiis possimus earum porro dolores sunt autem laboriosam esse provident obcaecati necessitatibus commodi, quos hic numquam qui explicabo?',
-    membership: {
-      user: {
-        id: 'user1',
-        name: 'Farid',
-        identifier: 'farid'
-      },
-
-      station: {
-        id: 'station1',
-        name: 'Bayern',
-        identifier: 'bayern'
-      }
-    },
-    votes: [
-      {
-        id: 'vote1',
-        membership: {
-          user: {
-            id: 'user1',
-            name: 'Farid',
-            identifier: 'farid'
-          }
-        },
-        type: 'UPVOTE'
-      },
-      {
-        id: 'vote2',
-        membership: {
-          user: {
-            id: 'user2',
-            name: 'Mohammed',
-            identifier: 'mohammed'
-          }
-        },
-        type: 'UPVOTE'
-      }
-    ],
-    createdAt: moment(0).toISOString(),
-    updatedAt: moment(0).toISOString()
-  }
-
   return (
     <div id='home'>
       <Nav />
 
       <main className='w-screen'>
-        <Topics sort='HOT' dateRange='MONTH' />
+        <Route path='/explore' component={Explore} />
       </main>
     </div>
   )
