446fa8aec42fe346b8b5481a18ad95883f340ab5
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-03-14 13:11:14 +0200

    created better semantic Topic component

--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useCookies } from 'react-cookie'
-
+import moment from 'moment'
 import { Topic as ITopic } from '../interfaces/Topic'
 import { User } from '../interfaces/User'
 import {
@@ -37,60 +37,96 @@ const Topic = ({ topic }: { topic: ITopic }) => {
   const downVotes = votes.filter(vote => vote.type === 'DOWNVOTE')
   const votesCount = upVotes.length - downVotes.length
 
-  console.log('topic', topic)
+  // Date and Time
+  const date = moment(topic.createdAt).format('Do MMM YYYY')
+  const time = moment(topic.createdAt).format('LT')
 
-  // return <h1>{topic.content}</h1>
   return (
-    <Box className='border w-1/3 p-2 rounded-md'>
-      <Stack>
-        <Stack isInline spacing={2}>
-          <Stack spacing={2} justify='between'>
-            <IconButton
-              aria-label='upvote'
-              icon={TiArrowUpThick}
-              variantColor={userVote?.type === 'UPVOTE' ? 'blue' : 'gray'}
-            />
-            <Text className='text-center font-bold text-lg'>{votesCount}</Text>
-            <IconButton
-              aria-label='downvote'
-              icon={TiArrowDownThick}
-              variantColor={userVote?.type === 'DOWNVOTE' ? 'red' : 'gray'}
-            />
-          </Stack>
+    <article className='topic'>
+      <aside>
+        <button>Upvote</button>
+        <p>{votesCount}</p>
+        <button>Downvote</button>
+      </aside>
 
-          <Stack spacing={2}>
-            <Stack isInline spacing={1}>
-              <LinkButton to={`/s/${topic.station.identifier}`} variant='link'>
-                {topic.station.name}
-              </LinkButton>
-              <Text>•</Text>
-              <LinkButton to={`/u/${topic.user.identifier}`} variant='link'>
-                {topic.user.name}
-              </LinkButton>
-            </Stack>
+      <header>
+        <h6>
+          <strong>{topic.station.name}</strong> •{' '}
+          <strong>{topic.user.name}</strong> •{' '}
+          <strong className='date'>
+            {date} <span className='time'>{time}</span>
+          </strong>
+        </h6>
+        <h3>{topic.title}</h3>
+      </header>
 
-            <Heading size='md'>{topic.title}</Heading>
-            <Text>
-              {topic.content.length > 120
-                ? topic.content.substr(0, 120) + '...'
-                : topic.content}
-            </Text>
-          </Stack>
-        </Stack>
-        <Flex>
-          <Button roundedRight='0' className='flex-grow' leftIcon={TiMessage}>
-            Comments
-          </Button>
-          <Button rounded='0' className='flex-grow' leftIcon={TiLocationArrow}>
-            Share
-          </Button>
-          <Button roundedLeft='0' className='flex-grow' leftIcon={TiInfo}>
-            Report
-          </Button>
-        </Flex>
-      </Stack>
-    </Box>
+      <main>
+        <p>
+          {topic.content.length > 120
+            ? topic.content.substr(0, 120) + '...'
+            : topic.content}
+        </p>
+      </main>
+
+      <footer>
+        <button>Comments</button>
+        <button>Share</button>
+        <button>Report</button>
+      </footer>
+    </article>
   )
+  // return <h1>{topic.content}</h1>
+  // return (
+  //   <Box className='border w-1/3 p-2 rounded-md'>
+  //     <Stack>
+  //       <Stack isInline spacing={2}>
+  //         <Stack spacing={2} justify='between'>
+  //           <IconButton
+  //             aria-label='upvote'
+  //             icon={TiArrowUpThick}
+  //             variantColor={userVote?.type === 'UPVOTE' ? 'blue' : 'gray'}
+  //           />
+  //           <Text className='text-center font-bold text-lg'>{votesCount}</Text>
+  //           <IconButton
+  //             aria-label='downvote'
+  //             icon={TiArrowDownThick}
+  //             variantColor={userVote?.type === 'DOWNVOTE' ? 'red' : 'gray'}
+  //           />
+  //         </Stack>
+
+  //         <Stack spacing={2}>
+  //           <Stack isInline spacing={1}>
+  //             <LinkButton to={`/s/${topic.station.identifier}`} variant='link'>
+  //               {topic.station.name}
+  //             </LinkButton>
+  //             <Text>•</Text>
+  //             <LinkButton to={`/u/${topic.user.identifier}`} variant='link'>
+  //               {topic.user.name}
+  //             </LinkButton>
+  //           </Stack>
+
+  //           <Heading size='md'>{topic.title}</Heading>
+  //           <Text>
+  //             {topic.content.length > 120
+  //               ? topic.content.substr(0, 120) + '...'
+  //               : topic.content}
+  //           </Text>
+  //         </Stack>
+  //       </Stack>
+  //       <Flex>
+  //         <Button roundedRight='0' className='flex-grow' leftIcon={TiMessage}>
+  //           Comments
+  //         </Button>
+  //         <Button rounded='0' className='flex-grow' leftIcon={TiLocationArrow}>
+  //           Share
+  //         </Button>
+  //         <Button roundedLeft='0' className='flex-grow' leftIcon={TiInfo}>
+  //           Report
+  //         </Button>
+  //       </Flex>
+  //     </Stack>
+  //   </Box>
+  // )
 }
 
 export default Topic
