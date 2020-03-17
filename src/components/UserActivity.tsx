0dc56546890d12e6ba146e7a3c017778b3b40be7
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-03-17 12:59:38 +0200

    combined multiple cards into one simple mini activity card

--- src/components/UserActivity.tsx
+++ src/components/UserActivity.tsx
@@ -7,94 +7,64 @@ import { Vote } from '../interfaces/Vote'
 import LinkButton from './LinkButton'
 import useGradiant from '../hooks/useGradiant'
 
-const TopicCard = ({
-  topic,
-  background
-}: {
-  topic: Topic
-  background: string
-}) => {
-  const { station } = topic
-
-  return (
-    <div className={`card ${background}`}>
-      Posted{' '}
-      <LinkButton
-        variant='link'
-        to={`/g/${station?.identifier}?topic=${topic?.id}`}>
-        {topic?.title}
-      </LinkButton>
-      .
-    </div>
-  )
+function instanceOfTopic(object: any): object is Topic {
+  return 'title' in object
 }
 
-const CommentCard = ({
-  comment,
-  background
-}: {
-  comment: Comment
-  background: string
-}) => {
-  const { topic, station } = comment
-  return (
-    <div className={`card ${background}`}>
-      Commented on{' '}
-      <LinkButton
-        variant='link'
-        to={`/g/${station?.identifier}?topic=${topic?.id}`}>
-        {topic?.title}
-      </LinkButton>
-      .
-    </div>
-  )
+function instanceOfComment(object: any): object is Comment {
+  return 'content' in object
 }
 
-const VoteCard = ({ vote, background }: { vote: Vote; background: string }) => {
-  const { topic, station, type } = vote
-  const normalized = type.charAt(0) + type.slice(1).toLowerCase() + 'd'
+const MiniActivityCard = ({
+  background,
+  activity
+}: {
+  background: string
+  activity: Topic | Comment | Vote
+}) => {
+  const { station, createdAt } = activity
+  const topic = instanceOfTopic(activity) ? activity : activity.topic
+  const message = instanceOfTopic(activity)
+    ? 'Posted'
+    : instanceOfComment(activity)
+    ? 'Commented On'
+    : activity.type.charAt(0) + activity.type.slice(1).toLowerCase() + 'd'
+  const date = moment(createdAt).format('Do MMM YYYY LT')
 
   return (
     <div className={`card ${background}`}>
-      {normalized}{' '}
+      {message}{' '}
       <LinkButton
         variant='link'
         to={`/g/${station?.identifier}?topic=${topic?.id}`}>
         {topic?.title}
+      </LinkButton>{' '}
+      in
+      <LinkButton variant='link' to={`/g/${station?.identifier}`}>
+        {station?.name}
       </LinkButton>
-      .
+      <small className='date'>on {date}</small>.
     </div>
   )
 }
 
 const UserActivity = ({ profile }: { profile: User }) => {
-  const [[bg, shade]] = useGradiant()
+  const [[bg]] = useGradiant()
   const { topics = [], comments = [], votes = [] } = profile
 
   const activities = [...topics, ...comments, ...votes].sort(
-    (a, b) =>
-      moment(b.createdAt).milliseconds() - moment(a.createdAt).milliseconds()
+    (a, b) => moment(b.createdAt).unix() - moment(a.createdAt).unix()
   )
-
-  function instanceOfTopic(object: any): object is Topic {
-    return 'title' in object
-  }
-
-  function instanceOfComment(object: any): object is Comment {
-    return 'content' in object
-  }
 
   return (
     <div id='user-activity'>
-      {activities.map(activity =>
-        instanceOfTopic(activity) ? (
-          <TopicCard key={activity.id} topic={activity} background={bg} />
-        ) : instanceOfComment(activity) ? (
-          <CommentCard key={activity.id} comment={activity} background={bg} />
-        ) : (
-          <VoteCard key={activity.id} vote={activity} background={bg} />
-        )
-      )}
+      {activities.map(activity => (
+        <MiniActivityCard
+          key={activity.id}
+          background={bg}
+          activity={activity}
+        />
+      ))}
     </div>
   )
 }
