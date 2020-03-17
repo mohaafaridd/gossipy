6c4e597c72a4f8abb3fc2f7e3c06be07f15fb758
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-03-17 13:11:53 +0200

    added tooltips to user activities

--- src/components/UserActivity.tsx
+++ src/components/UserActivity.tsx
@@ -6,6 +6,7 @@ import { Comment } from '../interfaces/Comment'
 import { Vote } from '../interfaces/Vote'
 import LinkButton from './LinkButton'
 import useGradiant from '../hooks/useGradiant'
+import { Tooltip } from '@chakra-ui/core'
 
 function instanceOfTopic(object: any): object is Topic {
   return 'title' in object
@@ -30,19 +31,31 @@ const MiniActivityCard = ({
     ? 'Commented On'
     : activity.type.charAt(0) + activity.type.slice(1).toLowerCase() + 'd'
   const date = moment(createdAt).format('Do MMM YYYY LT')
+  const longName = (name: string = '') =>
+    name.length > 16 ? name.substr(0, 16) + '...' : name
+  const topicTitle = longName(topic?.title)
+  const stationName = longName(station?.name)
 
   return (
     <div className={`card ${background}`}>
       {message}{' '}
-      <LinkButton
-        variant='link'
-        to={`/g/${station?.identifier}?topic=${topic?.id}`}>
-        {topic?.title}
-      </LinkButton>{' '}
+      <Tooltip
+        aria-label={topic?.title || 'Topic Title'}
+        label={topic?.title || 'Topic Title'}>
+        <LinkButton
+          variant='link'
+          to={`/g/${station?.identifier}?topic=${topic?.id}`}>
+          {topicTitle}
+        </LinkButton>
+      </Tooltip>{' '}
       in
-      <LinkButton variant='link' to={`/g/${station?.identifier}`}>
-        {station?.name}
-      </LinkButton>
+      <Tooltip
+        aria-label={station?.name || 'Station name'}
+        label={station?.name || 'Station name'}>
+        <LinkButton variant='link' to={`/g/${station?.identifier}`}>
+          {stationName}
+        </LinkButton>
+      </Tooltip>
       <small className='date'>on {date}</small>.
     </div>
   )
