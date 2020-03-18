c7fe4605702af9ce6b5bfce58abde623357a2393
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-03-18 20:06:10 +0200

    linked join button with current membership state

--- src/components/StationInfo.tsx
+++ src/components/StationInfo.tsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import { useCountUp } from 'react-countup'
-import { Station } from '../interfaces/Station'
-import useGradiant from '../hooks/useGradiant'
+import React, { useState, useContext, useEffect } from 'react'
 import moment from 'moment'
+import { Station } from '../interfaces/Station'
+import { Membership } from '../interfaces/Membership'
+import useGradiant from '../hooks/useGradiant'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+import Loading from './Loading'
+import AuthContext from '../context/auth/authContext'
+import { Button } from '@chakra-ui/core'
+
+// const CREATE_MEMBERSHIP = gql``
+
+const GET_MEMBERSHIP = gql`
+  query getMembership($station: ID!) {
+    userMembership(station: $station) {
+      id
+      state
+      role
+    }
+  }
+`
+
+interface JoinState {
+  color: string
+  message: string
+  disabled: boolean
+}
+
+const generateJoinState = (
+  station: Station,
+  membership: Membership
+): JoinState => {
+  switch (membership.state) {
+    case 'PENDING':
+      return {
+        color: 'gray',
+        disabled: true,
+        message: 'Request Pending'
+      }
+
+    case 'BANNED':
+      return {
+        color: 'red',
+        disabled: true,
+        message: "You're banned"
+      }
+
+    default:
+      return {
+        color: 'green',
+        disabled: false,
+        message: `${station.public ? '' : 'Request '}Join`
+      }
+  }
+}
 
 const StationInfo = ({ station }: { station: Station }) => {
+  const authContext = useContext(AuthContext)
+
+  const [isActiveMember, setIsActiveMember] = useState(false)
+
+  const [joinState, setJoinState] = useState<JoinState>({
+    message: 'Join',
+    color: 'green',
+    disabled: false
+  })
+
+  const { data, loading, error } = useQuery(GET_MEMBERSHIP, {
+    variables: {
+      station: station.id
+    }
+  })
+
   const [, , [bg]] = useGradiant()
   const { topics, members } = station
 
@@ -12,17 +79,18 @@ const StationInfo = ({ station }: { station: Station }) => {
     membership => membership.state === 'ACTIVE'
   )
 
-  const { countUp: membershipsCounter } = useCountUp({
-    end: activeMembers?.length || 0,
-    duration: 2
-  })
-
-  const { countUp: topicsCounter } = useCountUp({
-    end: topics?.length || 0,
-    duration: 2
-  })
-
   const date = moment(station.createdAt).format('Do MMM YYYY')
+
+  useEffect(() => {
+    if (data) {
+      const { userMembership }: { userMembership: Membership } = data
+      const state = generateJoinState(station, userMembership)
+      setJoinState(state)
+      setIsActiveMember(userMembership.state === 'ACTIVE')
+    }
+  }, [data])
+
+  if (loading) return <Loading message='Loading Membership Information' />
 
   return (
     <div id='station-info' className={bg}>
@@ -32,12 +100,18 @@ const StationInfo = ({ station }: { station: Station }) => {
 
       <div className='counters'>
         <div className='counter'>
-          <div>{membershipsCounter} Members</div>
+          <div>{activeMembers?.length} Members</div>
         </div>
         <div className='counter'>
-          <div>{topicsCounter} Topics</div>
+          <div>{topics?.length} Topics</div>
         </div>
       </div>
+
+      {authContext.authenticated && !isActiveMember && (
+        <Button variantColor={joinState.color} isDisabled={joinState.disabled}>
+          {joinState.message}
+        </Button>
+      )}
     </div>
   )
 }
