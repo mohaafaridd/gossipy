8288f5b2d87a7e7ad1650076ca13295fd9f89ea1
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-04-07 18:12:34 +0200

    change karma class into active class in user info

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -4,9 +4,12 @@ import { User } from '../interfaces/User'
 import { Avatar } from '@chakra-ui/core'
 import useKarma from '../hooks/useKarma'
 import useGradient from '../hooks/useGradient'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 const UserInfo = ({ profile }: { profile: User }) => {
+  let history = useHistory()
+  let pathname = history.location.pathname.split('/')
+
   const karma = useKarma(profile.karma)
   const { countUp: karmaCounter } = useCountUp({ end: karma, duration: 2 })
   const { countUp: topicsCounter } = useCountUp({
@@ -34,25 +37,35 @@ const UserInfo = ({ profile }: { profile: User }) => {
       <h2>{profile.name}</h2>
 
       <div className='counters'>
-        <div className='karma counter'>
+        <div
+          className={`counter ${
+            pathname.includes('activities') ? 'active' : ''
+          }`}>
           <Link to={`/u/${profile.identifier}/activities`}>
             <div>{karmaCounter}</div>
             Karma
           </Link>
         </div>
 
-        <div className='counter karma'>
+        <div
+          className={`counter ${pathname.includes('topics') ? 'active' : ''}`}>
           <Link to={`/u/${profile.identifier}/topics`}>
             <div>{topicsCounter}</div>
             Topics
           </Link>
         </div>
 
-        <div className='counter'>
+        <div
+          className={`counter ${
+            pathname.includes('comments') ? 'active' : ''
+          }`}>
           <div>{commentsCounter}</div>Comments
         </div>
 
-        <div className='counter'>
+        <div
+          className={`counter ${
+            pathname.includes('subscription') ? 'active' : ''
+          }`}>
           <div>{membershipsCounter}</div>Subscription
         </div>
       </div>
