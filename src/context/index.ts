10804f2fb8c48489e534ad71930a9bd7d682aad0
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-04-27 07:09:06 +0200

    created index files for contexts and interfaces

---
+++ src/context/index.ts
@@ -0,0 +1,32 @@
+import AuthContext from './auth/authContext'
+import AuthState from './auth/AuthState'
+
+import CommentContext from './comment/commentContext'
+import CommentState from './comment/CommentState'
+
+import MembershipContext from './membership/membershipContext'
+import MembershipState from './membership/MembershipState'
+
+import StationContext from './station/stationContext'
+import StationState from './station/StationState'
+
+import TagContext from './tag/tagContext'
+import TagState from './tag/TagState'
+
+import TopicContext from './topic/topicContext'
+import TopicState from './topic/TopicState'
+
+export {
+  AuthContext,
+  AuthState,
+  CommentContext,
+  CommentState,
+  MembershipContext,
+  MembershipState,
+  StationContext,
+  StationState,
+  TagContext,
+  TagState,
+  TopicContext,
+  TopicState
+}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,24 @@
+import { Comment } from './Comment'
+import { Membership, MembershipState, Role } from './Membership'
+import { Search } from './Search'
+import { Station } from './Station'
+import { Tag } from './Tag'
+import { DateRange, SortType, Topic } from './Topic'
+import { User } from './User'
+import { Vote, VoteType } from './Vote'
+
+export type{
+  Comment,
+  Membership,
+  MembershipState,
+  Role,
+  Search,
+  Station,
+  Tag,
+  DateRange,
+  SortType,
+  Topic,
+  User,
+  Vote,
+  VoteType
+}
