b568f22bfd368b744bc4bf73c1c31925f5878c3d
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-25 11:31:44 +0200

    returned react default public folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { CookiesProvider } from 'react-cookie'
 import Context from './Context'
+import { CSSReset } from '@chakra-ui/core'
 
 import Apollo from './Apollo'
 import Chakra from './chakra'
 import App from './App'
 import './styles/tailwind.css'
-import { CSSReset } from '@chakra-ui/core'
 
 ReactDOM.render(
   <CookiesProvider>
